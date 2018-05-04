3462b40a393e0e85ea844d91505cedf04752faf4
lipeiyuan <lipeiyuan@sanduspace.cn>
2018-05-04 16:41:37 +0800

    初始化

--- src/main/java/com/lpy/register/RegisterApplication.java
+++ src/main/java/com/lpy/register/RegisterApplication.java
@@ -1,4 +1,4 @@
-package com.lpy.register.register;
+package com.lpy.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
